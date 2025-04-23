fd5caecadfbe773eb00b1cbb247e9822fb013b58
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-04-23 10:14:30 -0300

    Criador estrutura inicial de todas as classes do projeto

--- src/br/dev/kauan/ConversorTemperatura/gui/TelaConversor.java
+++ src/br/dev/kauan/ConversorTemperatura/gui/TelaConversor.java
@@ -1,5 +1,24 @@
 package br.dev.kauan.ConversorTemperatura.gui;
 
-public class TelaConversor {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+public class TelaConversor {
+	// Atributo Caixa de texto
+	private JTextField textCelsius;
+	
+	// Atributo Caixa de informação
+	private JLabel labelCelsius;
+	private JLabel labelResultado;
+	private JLabel labelMensagemErro;
+	
+	// Atributo Botoes de conversão
+	private JButton buttonKelvin;
+	private JButton buttonFahreinheit;
+	
+	// Métodos
+	public void criarTelaConversor() {
+		
+	}
 }
--- src/br/dev/kauan/ConversorTemperatura/model/Temperatura.java
+++ src/br/dev/kauan/ConversorTemperatura/model/Temperatura.java
@@ -1,5 +1,27 @@
 package br.dev.kauan.ConversorTemperatura.model;
 
 public class Temperatura {
-
+	//Atributo
+	private double celsius;
+	
+	//Métodos
+	
+	public double getCelsius() {
+		return celsius;
+		
+	}
+	
+	public void setCelsius(double celsius) {
+		this.celsius = celsius;
+	}
+	
+	public double converterParaKelvin() {
+		double kelvin = 0;
+		return kelvin;
+	}
+	
+	public double converterParaFahreinhet() {
+		double fahreinhet = 0;
+		return fahreinhet;
+	}
 }
